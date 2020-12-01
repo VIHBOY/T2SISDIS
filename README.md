834d9c69dd3d0fa39a9a4594e8c2ee066bbd802c
Renato Bassi <renato.bassi@sansano.usm.cl>
2020-12-01 17:29:21 -0300

    Final Maquina con README

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# T2SISDIS
+# Tarea 2 Sistemas Dsitribuidos
+
+
+## Comenzando 🚀
+
+_Estas instrucciones te permitirán obtener una copia del proyecto en funcionamiento en tus máquinas virtuales para propósitos de desarrollo y pruebas._
+
+### Pre-requisitos 📋
+
+_Que cosas necesitas para instalar el software y como instalarlas_
+
+```
+Las maquinas virtuales ya tienen todo lo necesario para realizar pruebas.
+```
+
+### Pasos a Seguir 🔧
+
+_Se debe seguir estos pasos para poder ejecutar la tarea_
+
+```
+1. En la Maquina dist28, se debe ejecutar make
+
+Lo que le otorgara a la maquina el rol de NameNode
+
+2. En la Maquina dist27, se debe ejecutar make
+
+Lo que le otorgara a la maquina el rol de DataNode3
+
+3. En la Maquina dist26, se debe ejecutar make
+
+Lo que le otorgara a la maquina el rol de Datanode2
+
+4. En la Maquina dist27, se debe ejecutar make
+
+Lo que le otorgara a la maquina el rol de DataNode1, esta posee tanto cliente uploader como downloader.
+```
+Existe un  archivo llamado clienteseparado.go el cual posee los clientes uploader y downloader para probar solicitudes al mismo tiempo
+
+## Consideraciones NameNode ⚙️
+
+En todo momento se debe mantener encendido el servidor.
+
+## Consideraciones Datanode3 ⚙️
+
+La forma en que esta implementado el aspecto de Datanode3 permite que esta este apagada y siga funcionando el procedimiento de la tarea.
+
+## Consideraciones Datanode2 ⚙️
+
+La forma en que esta implementado el aspecto de Datanode2 permite que esta este apagada y siga funcionando el procedimiento de la tarea.
+
+## Consideraciones Datanode1 ⚙️
+
+En todo momento se debe mantener encendido el servidor, dado que esta posee los clientes de uploader y downloader.
+
+
+Aclaraciones.
+
+
+Existe un menú en Datanode1 el cual pregunta por si desea subir o bajar un archivo, cabe recalcar que como se trata de libros, estos deben estar en formato pdf y no tener guion bajos en su nombre, el archivo debe estar en la maquina 25 para ser subido, y este será bajado con el nombre con un 1 añadido en su titulo.
+
+Como se indico antes, existe un archivo llamado clienteseparado.go en la maquina 25 el cual se puede utilizar para probar conexiones simultaneas, cabe decir que el datanode1 no puede caerse (el datanode 2 y el 3 si) es una de las pocas indicaciones que damos.
+
+
+
+## Construido con
+
+[VS CODE] - Editor de texto
+
+## Autores
+
+**Joaquin Concha** - 201773569-4 *VIHBOY*
+**Renato Bassi** - 201773521-k *bassisi*
---
+++ clienteseparado.go
@@ -0,0 +1,567 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/VIHBOY/T2SISDIS/chat"
+	"google.golang.org/grpc"
+)
+
+func do3(text string, c chat.ChatServiceClient, c2 chat.ChatServiceClient, c3 chat.ChatServiceClient) {
+
+	/*var conn3 *grpc.ClientConn
+	conn3, err3 := grpc.Dial(":9003", grpc.WithInsecure())
+	if err3 != nil {
+		log.Fatalf("uwu %s", err3)
+	}
+	c3 := chat.NewChatServiceClient(conn3)
+
+	defer conn3.Close()*/
+	Nodisponible2 := []int32{}
+	Sidisponible2 := []int32{}
+
+	message2 := chat.Message{
+		Body: "u2u",
+	}
+	r1, _ := c.SayHello2(context.Background(), &message2)
+	fmt.Println(r1)
+	if r1 == nil {
+		Nodisponible2 = append(Nodisponible2, 1)
+	} else {
+		Sidisponible2 = append(Sidisponible2, 1)
+	}
+	r, _ := c2.SayHello2(context.Background(), &message2)
+	fmt.Println(r)
+	if r == nil {
+		Nodisponible2 = append(Nodisponible2, 2)
+	} else {
+		Sidisponible2 = append(Sidisponible2, 2)
+	}
+	r2, _ := c3.SayHello2(context.Background(), &message2)
+	if r2 == nil {
+		Nodisponible2 = append(Nodisponible2, 3)
+	} else {
+		Sidisponible2 = append(Sidisponible2, 3)
+	}
+
+	fileToBeChunked := "./" + text + ".pdf"
+	titulo := text
+	file, err := os.Open(fileToBeChunked)
+	rand.Seed(time.Now().UnixNano())
+	max := len(Sidisponible2)
+	chosendn := Sidisponible2[rand.Intn(max)]
+	/*rand.Intn(max-min) + min*/
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer file.Close()
+
+	fileInfo, _ := file.Stat()
+
+	var fileSize int64 = fileInfo.Size()
+
+	const fileChunk = 256000
+
+	// calculate total number of parts the file will be chunked into
+
+	totalPartsNum := uint64(math.Ceil(float64(fileSize) / float64(fileChunk)))
+	fmt.Printf("Dividir en  %d Chunks.\n", totalPartsNum)
+
+	for i := uint64(0); i < totalPartsNum; i++ {
+
+		partSize := int(math.Min(fileChunk, float64(fileSize-int64(i*fileChunk))))
+		partBuffer := make([]byte, partSize)
+
+		file.Read(partBuffer)
+
+		message := chat.Response{
+			Info:      strconv.FormatUint(i, 10),
+			Name:      titulo,
+			Elegido:   1,
+			Cantidad:  totalPartsNum,
+			FileChunk: partBuffer,
+		}
+
+		switch chosendn {
+		case 1:
+			c.SayHello(context.Background(), &message)
+		case 2:
+			c2.SayHello(context.Background(), &message)
+		case 3:
+			c3.SayHello(context.Background(), &message)
+		}
+	}
+	fmt.Println(chosendn)
+	Message_totalchunks := chat.Message{
+		In:           int32(totalPartsNum),
+		Nodisponible: Nodisponible2,
+	}
+	var propuesta *chat.Propuesta
+	switch chosendn {
+	case 1:
+		propuesta, _ = c.GenerarPropuesta2(context.Background(), &Message_totalchunks)
+	case 2:
+		propuesta, _ = c2.GenerarPropuesta2(context.Background(), &Message_totalchunks)
+	case 3:
+		propuesta, _ = c3.GenerarPropuesta2(context.Background(), &Message_totalchunks)
+	}
+	propuesta.Titulo = titulo
+	propuesta.In = int32(chosendn)
+	propuesta.Nodisponible = Nodisponible2
+
+	var respuesta *chat.Message
+	switch chosendn {
+	case 1:
+		respuesta, _ = c.PedirConfirmacionNM(context.Background(), propuesta)
+	case 2:
+		respuesta, _ = c2.PedirConfirmacionNM(context.Background(), propuesta)
+	case 3:
+		respuesta, _ = c3.PedirConfirmacionNM(context.Background(), propuesta)
+
+	}
+	fmt.Println("uwu")
+	fmt.Println(propuesta.Pos)
+	fmt.Println(propuesta.L1)
+	fmt.Println(propuesta.L2)
+	fmt.Println(propuesta.L3)
+
+	_ = respuesta
+	/*
+		if respuesta.In == 1 {
+			fmt.Println("Propuesta Aceptada")
+		}
+	*/
+
+	for {
+		if respuesta.In == 1 {
+			fmt.Println("Propuesta Aceptada")
+			break
+		} else {
+			fmt.Println("Propuesta No Aceptada")
+			fmt.Println(respuesta.Nodisponible)
+			Message_totalchunks := chat.Message{
+				In:           int32(totalPartsNum),
+				Nodisponible: respuesta.Nodisponible,
+			}
+			switch chosendn {
+			case 1:
+				propuesta, _ = c.GenerarPropuesta2(context.Background(), &Message_totalchunks)
+			case 2:
+				propuesta, _ = c2.GenerarPropuesta2(context.Background(), &Message_totalchunks)
+			case 3:
+				propuesta, _ = c3.GenerarPropuesta2(context.Background(), &Message_totalchunks)
+			}
+
+			propuesta.Titulo = titulo
+			propuesta.In = int32(chosendn)
+			propuesta.Nodisponible = respuesta.Nodisponible
+			var respuesta *chat.Message
+			switch chosendn {
+			case 1:
+				respuesta, _ = c.PedirConfirmacionNM(context.Background(), propuesta)
+			case 2:
+				respuesta, _ = c2.PedirConfirmacionNM(context.Background(), propuesta)
+			case 3:
+				respuesta, _ = c3.PedirConfirmacionNM(context.Background(), propuesta)
+
+			}
+			fmt.Println(respuesta.In)
+			if respuesta.In == 1 {
+				break
+			}
+		}
+	}
+
+	fmt.Println(propuesta.Pos)
+	fmt.Println(propuesta.L1)
+	fmt.Println(propuesta.L2)
+	fmt.Println(propuesta.L3)
+
+	switch chosendn {
+	case 1:
+		respuesta, _ = c.EscribirPropuesta(context.Background(), propuesta)
+	case 2:
+		respuesta, _ = c2.EscribirPropuesta(context.Background(), propuesta)
+	case 3:
+		respuesta, _ = c3.EscribirPropuesta(context.Background(), propuesta)
+
+	}
+
+	Agregar := chat.Message{
+		Body: text,
+	}
+	fmt.Println("Holi2")
+	var Titulos *chat.Titulos
+	switch chosendn {
+	case 1:
+		c.Repartir(context.Background(), propuesta)
+		c.AgregarTitulo(context.Background(), &Agregar)
+		Titulos, _ = c.VerTitulos(context.Background(), &Agregar)
+	case 2:
+		c2.Repartir(context.Background(), propuesta)
+		c2.AgregarTitulo(context.Background(), &Agregar)
+		Titulos, _ = c2.VerTitulos(context.Background(), &Agregar)
+
+	case 3:
+		c3.Repartir(context.Background(), propuesta)
+		c3.AgregarTitulo(context.Background(), &Agregar)
+		Titulos, _ = c3.VerTitulos(context.Background(), &Agregar)
+	}
+	fmt.Println(Titulos)
+	fmt.Printf("Titulos Disponibles\n")
+	for _, a := range Titulos.Titulos {
+		fmt.Printf("Titulos: %s\n", a)
+	}
+}
+
+func do2(text string, c chat.ChatServiceClient, c2 chat.ChatServiceClient, c3 chat.ChatServiceClient) {
+	Agregar := chat.Message{
+		Body: "text",
+	}
+	Titulos, _ := c.VerTitulos(context.Background(), &Agregar)
+	fmt.Printf("Titulos Disponibles\n")
+	for _, a := range Titulos.Titulos {
+		fmt.Printf("Titulos: %s\n", a)
+	}
+	fmt.Println("Ingrese Titulo")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Ingrese Archivo a bajar")
+	text2, _ := reader.ReadString('\n')
+	// convert CRLF to LF
+	text3 := strings.Replace(text2, "\n", "", -1)
+	fmt.Println(text3)
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial(":9004", grpc.WithInsecure())
+	c4 := chat.NewChatServiceClient(conn)
+
+	if err != nil {
+		log.Fatalf("uwu %s", err)
+	}
+	fmt.Println(text2)
+	Agregar2 := chat.Message{
+		Body: text3,
+	}
+	Ti, _ := c4.ObtenerUbicaciones(context.Background(), &Agregar2)
+	fmt.Println(Ti)
+	c.BuscarChunks(context.Background(), Ti)
+	me := chat.Message{
+		Body: text3,
+		In:   int32(len(Ti.Titulos)),
+	}
+	c.Unir(context.Background(), &me)
+	defer conn.Close()
+
+}
+func do(text string, c chat.ChatServiceClient, c2 chat.ChatServiceClient, c3 chat.ChatServiceClient) {
+
+	/*var conn3 *grpc.ClientConn
+	conn3, err3 := grpc.Dial(":9003", grpc.WithInsecure())
+	if err3 != nil {
+		log.Fatalf("uwu %s", err3)
+	}
+	c3 := chat.NewChatServiceClient(conn3)
+
+	defer conn3.Close()*/
+	Nodisponible2 := []int32{}
+	Sidisponible2 := []int32{}
+
+	message2 := chat.Message{
+		Body: "u2u",
+	}
+	r1, _ := c.SayHello2(context.Background(), &message2)
+	if r1 == nil {
+		Nodisponible2 = append(Nodisponible2, 1)
+	} else {
+		Sidisponible2 = append(Sidisponible2, 1)
+	}
+	r, _ := c2.SayHello2(context.Background(), &message2)
+	if r == nil {
+		Nodisponible2 = append(Nodisponible2, 2)
+	} else {
+		Sidisponible2 = append(Sidisponible2, 2)
+	}
+	r2, _ := c3.SayHello2(context.Background(), &message2)
+	if r2 == nil {
+		Nodisponible2 = append(Nodisponible2, 3)
+	} else {
+		Sidisponible2 = append(Sidisponible2, 3)
+	}
+
+	fileToBeChunked := "./" + text + ".pdf"
+	titulo := text
+	file, err := os.Open(fileToBeChunked)
+	rand.Seed(time.Now().UnixNano())
+	max := len(Sidisponible2)
+	chosendn := Sidisponible2[rand.Intn(max)]
+	/*rand.Intn(max-min) + min*/
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer file.Close()
+
+	fileInfo, _ := file.Stat()
+
+	var fileSize int64 = fileInfo.Size()
+
+	const fileChunk = 256000
+
+	// calculate total number of parts the file will be chunked into
+
+	totalPartsNum := uint64(math.Ceil(float64(fileSize) / float64(fileChunk)))
+	fmt.Println("")
+
+	fmt.Printf("Dividiendo el archivo en %d Chunks. Agradecemos su espera\n", totalPartsNum)
+
+	for i := uint64(0); i < totalPartsNum; i++ {
+
+		partSize := int(math.Min(fileChunk, float64(fileSize-int64(i*fileChunk))))
+		partBuffer := make([]byte, partSize)
+
+		file.Read(partBuffer)
+
+		message := chat.Response{
+			Info:      strconv.FormatUint(i, 10),
+			Name:      titulo,
+			Elegido:   1,
+			Cantidad:  totalPartsNum,
+			FileChunk: partBuffer,
+		}
+
+		switch chosendn {
+		case 1:
+			c.SayHello(context.Background(), &message)
+		case 2:
+			c2.SayHello(context.Background(), &message)
+		case 3:
+			c3.SayHello(context.Background(), &message)
+		}
+	}
+	fmt.Println("")
+
+	Message_totalchunks := chat.Message{
+		In:           int32(totalPartsNum),
+		Nodisponible: Nodisponible2,
+	}
+	var propuesta *chat.Propuesta
+	switch chosendn {
+	case 1:
+		propuesta, _ = c.GenerarPropuesta(context.Background(), &Message_totalchunks)
+	case 2:
+		propuesta, _ = c2.GenerarPropuesta(context.Background(), &Message_totalchunks)
+	case 3:
+		propuesta, _ = c3.GenerarPropuesta(context.Background(), &Message_totalchunks)
+	}
+	fmt.Println(propuesta.Pos)
+	fmt.Println(Nodisponible2)
+	propuesta.Titulo = titulo
+	propuesta.In = int32(chosendn)
+	propuesta.Nodisponible = Nodisponible2
+
+	var respuesta *chat.Message
+	switch chosendn {
+	case 1:
+		respuesta, _ = c.PedirConfirmacion2(context.Background(), propuesta)
+	case 2:
+		respuesta, _ = c2.PedirConfirmacion2(context.Background(), propuesta)
+	case 3:
+		respuesta, _ = c3.PedirConfirmacion2(context.Background(), propuesta)
+
+	}
+	/*fmt.Println(propuesta.Pos)
+	fmt.Println(propuesta.L1)
+	fmt.Println(propuesta.L2)
+	fmt.Println(propuesta.L3)*/
+
+	_ = respuesta
+	/*
+		if respuesta.In == 1 {
+			fmt.Println("Propuesta Aceptada")
+		}
+	*/
+
+	for {
+		if respuesta.In == 1 {
+			fmt.Println("")
+			fmt.Println("Propuesta Aceptada")
+			fmt.Println("")
+
+			break
+		} else {
+			fmt.Println("")
+
+			fmt.Println("Propuesta No Aceptada")
+			fmt.Println("")
+			Message_totalchunks := chat.Message{
+				In:           int32(totalPartsNum),
+				Nodisponible: respuesta.Nodisponible,
+			}
+			switch chosendn {
+			case 1:
+				propuesta, _ = c.GenerarPropuesta2(context.Background(), &Message_totalchunks)
+			case 2:
+				propuesta, _ = c2.GenerarPropuesta2(context.Background(), &Message_totalchunks)
+			case 3:
+				propuesta, _ = c3.GenerarPropuesta2(context.Background(), &Message_totalchunks)
+			}
+
+			propuesta.Titulo = titulo
+			propuesta.In = int32(chosendn)
+			propuesta.Nodisponible = respuesta.Nodisponible
+			var respuesta *chat.Message
+			switch chosendn {
+			case 1:
+				respuesta, _ = c.PedirConfirmacion2(context.Background(), propuesta)
+			case 2:
+				respuesta, _ = c2.PedirConfirmacion2(context.Background(), propuesta)
+			case 3:
+				respuesta, _ = c3.PedirConfirmacion2(context.Background(), propuesta)
+
+			}
+			if respuesta.In == 1 {
+				break
+			}
+		}
+	}
+	fmt.Println(propuesta.Pos)
+	fmt.Println(Nodisponible2)
+	fmt.Println("")
+	fmt.Println("Escribiendo Propuesta en Log.txt Ubicado en otra maquina (magia!:D)")
+	fmt.Println("")
+
+	switch chosendn {
+	case 1:
+		respuesta, _ = c.EscribirPropuestaDis(context.Background(), propuesta)
+	case 2:
+		respuesta, _ = c2.EscribirPropuestaDis(context.Background(), propuesta)
+	case 3:
+		respuesta, _ = c3.EscribirPropuestaDis(context.Background(), propuesta)
+
+	}
+
+	Agregar := chat.Message{
+		Body: text,
+	}
+	fmt.Println("")
+
+	fmt.Println("El nodo aleatorio Asigando comenzara a repartir uwu")
+	fmt.Println("")
+
+	var Titulos *chat.Titulos
+	switch chosendn {
+	case 1:
+		c.Repartir(context.Background(), propuesta)
+		c.AgregarTitulo(context.Background(), &Agregar)
+		Titulos, _ = c.VerTitulos(context.Background(), &Agregar)
+	case 2:
+		c2.Repartir(context.Background(), propuesta)
+		c2.AgregarTitulo(context.Background(), &Agregar)
+		Titulos, _ = c2.VerTitulos(context.Background(), &Agregar)
+
+	case 3:
+		c3.Repartir(context.Background(), propuesta)
+		c3.AgregarTitulo(context.Background(), &Agregar)
+		Titulos, _ = c3.VerTitulos(context.Background(), &Agregar)
+	}
+	fmt.Println("")
+
+	fmt.Printf("Titulos Disponibles\n")
+	for _, a := range Titulos.Titulos {
+		fmt.Printf("Titulos: %s\n", a)
+	}
+}
+
+func main() {
+
+	var iniciar chat.Message
+	iniciar.Body = "0"
+
+	var conn2 *grpc.ClientConn
+	conn2, err2 := grpc.Dial(":9001", grpc.WithInsecure())
+	fmt.Println(err2)
+
+	if err2 != nil {
+		log.Fatalf("uwu %s", err2)
+	}
+	c2 := chat.NewChatServiceClient(conn2)
+	c2.CambiarRA(context.Background(), &iniciar)
+
+	defer conn2.Close()
+
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial(":9000", grpc.WithInsecure())
+	fmt.Println(err)
+	if err != nil {
+		log.Fatalf("uwu %s", err)
+	}
+	c := chat.NewChatServiceClient(conn)
+	c.CambiarRA(context.Background(), &iniciar)
+
+	defer conn.Close()
+
+	var conn3 *grpc.ClientConn
+	conn3, err3 := grpc.Dial(":9002", grpc.WithInsecure())
+	fmt.Println(err3)
+
+	if err3 != nil {
+		log.Fatalf("uwu %s", err3)
+	}
+	c3 := chat.NewChatServiceClient(conn3)
+	c3.CambiarRA(context.Background(), &iniciar)
+
+	defer conn3.Close()
+	for {
+		fmt.Println("-------------------------------------------")
+		fmt.Println("Menú")
+		fmt.Println("")
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println("Ingrese que desea hacer")
+		fmt.Println("1. Subir")
+		fmt.Println("2. Bajar")
+		text, _ := reader.ReadString('\n')
+		// convert CRLF to LF
+		text = strings.Replace(text, "\n", "", -1)
+		switch text {
+		case "1":
+			fmt.Println("Ingrese Algoritmo a usar")
+			fmt.Println("1. Distribuido")
+			fmt.Println("2. Centralizado")
+			text2, _ := reader.ReadString('\n')
+			// convert CRLF to LF
+			text3 := strings.Replace(text2, "\n", "", -1)
+			switch text3 {
+			case "1":
+				fmt.Println("Ingrese Nombre de archivo")
+				text2, _ := reader.ReadString('\n')
+				// convert CRLF to LF
+				text3 = strings.Replace(text2, "\n", "", -1)
+				do(text3, c, c2, c3)
+			case "2":
+				fmt.Println("Ingrese Nombre de archivo")
+				text2, _ := reader.ReadString('\n')
+				// convert CRLF to LF
+				text3 = strings.Replace(text2, "\n", "", -1)
+				do3(text3, c, c2, c3)
+			}
+		case "2":
+
+			do2(text, c, c2, c3)
+		}
+
+	}
+
+}
